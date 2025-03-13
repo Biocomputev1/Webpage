a1e1921a2b83df70c6f897bfa2d0d8a64bd1d493
Biocomputev1 <electronics@biocomputeinc.com>
2025-03-13 16:38:32 +0530

    Refactor blog section styling for improved consistency and visual appeal

--- src/app/sections/blog.tsx
+++ src/app/sections/blog.tsx
@@ -9,17 +9,17 @@ export const SubstackPosts = () => {
         
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Left Card */}
-          <div className="ml-4 border-2 border-purple-300 rounded-xl w-auto  h-auto overflow-hidden shadow-md transition delay-50 duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg">
-          <Link href = "https://www.biocomputeinc.com/p/coming-soon?utm_campaign=post&utm_medium=web">
-            <Image src = {blogimage1} alt="BioCompute Logo"/>
+          <div className="ml-4 border-2 border-neutral-400 rounded-xl w-auto  h-auto overflow-hidden shadow-md transition delay-50 duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg">
+            <Link href = "https://www.biocomputeinc.com/p/coming-soon?utm_campaign=post&utm_medium=web">
+            <Image src = {blogimage1} alt="BioCompute Logo" className="border-2 rounded-xl border-neutral-300"/>
             <h2 className="text-xl font-inter mt-6 mb-2 mx-10">Hello World, We Are BioCompute!</h2>
             <p className="text-gray-600 mb-4 mx-10">Prologue</p>
             </Link>
           </div>
           {/* Right Card */}
-          <div className="mr-4 border-2 border-purple-300 bg-white rounded-xl overflow-hidden shadow-md transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg">
+          <div className="mr-4 border-2 border-neutral-400 bg-white rounded-xl overflow-hidden shadow-md transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-lg">
             <Link href = "https://www.biocomputeinc.com/p/a-primer-on-dna-no-pun-intended?utm_campaign=post&utm_medium=web">
-            <Image src={blogimage2} alt="DNA Double Helix"/>
+            <Image src={blogimage2} alt="DNA Double Helix" className='border-2 rounded-xl border-neutral-200'/>
             <h2 className="text-xl font-inter mt-6 mb-2 mx-10">A Primer on DNA (no pun intended)</h2>
             <p className="text-gray-600 mb-8 mx-10">It&apos;s been a week since we announced</p>
             <p className = "text-gray-600 -mt-8 mb-4 mx-10">BioCompute to the world</p>
