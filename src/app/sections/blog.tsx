f857ace99567b4ff326ce3b0bdac6c76a8c71350
Naveen-hehehe <itsnaveenmuthukrishnan@gmail.com>
2025-09-11 19:15:43 +0530

    Issue fixed

--- src/app/sections/blog.tsx
+++ src/app/sections/blog.tsx
@@ -11,7 +11,7 @@ export default function SubstackPosts ()  {
           {/* Left Card */}
           <div className=" border-2 border-neutral-400 rounded-xl w-auto h-auto overflow-hidden shadow-md transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:shadow-lg">
             <Link href = "https://blog.biocomputeinc.com/p/coming-soon">
-            <Image src = {blogimage1} alt="BioCompute Logo" className="border-2 rounded-xl border-neutral-300"/>
+            <Image src = {blogimage1} alt="BioCompute Logo" className="border-2 rounded-xl border-neutral-300 lg:w-full"/>
             <h2 className="text-xl font-inter mt-6 mb-2 mx-10">Hello World, We Are BioCompute!</h2>
             <p className="text-gray-600 mb-4 mx-10">Prologue</p>
             </Link>
@@ -19,7 +19,7 @@ export default function SubstackPosts ()  {
           {/* Right Card */}
           <div className="border-2 border-neutral-400 rounded-xl w-auto h-auto overflow-hidden shadow-md transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:shadow-lg">
             <Link href = "https://blog.biocomputeinc.com/p/a-primer-on-dna-no-pun-intended">
-            <Image src={blogimage2} alt="DNA Double Helix" className='border-2 rounded-xl border-neutral-300'/>
+            <Image src={blogimage2} alt="DNA Double Helix" className='border-2 rounded-xl border-neutral-300 lg:w-full'/>
             <h2 className="text-xl font-inter mt-6 mb-2 mx-10">A Primer on DNA (no pun intended)</h2>
             <p className="text-gray-600 mb-8 mx-10">It&apos;s been a week since we announced</p>
             <p className = "text-gray-600 -mt-8 mb-4 mx-10">BioCompute to the world</p>
